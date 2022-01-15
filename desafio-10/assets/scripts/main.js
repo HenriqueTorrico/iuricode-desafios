6d9bd0725e46034739cf09c3196b825959659fbc
HenriqueTorrico <henriquetorrico12@gmail.com>
2022-01-15 19:59:04 -0300

    desafios concluídos

---
+++ desafio-10/assets/scripts/main.js
@@ -0,0 +1,62 @@
+const scroll = [
+  {
+    imagem: "./assets/img/img-anime01.jpg",
+    titulo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eros tellus, malesuada et velit in, blandit molestie dolor.",
+    descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eros tellus, malesuada et velit in, blandit molestie dolor...",
+    data: "00/00/0000",
+    link: ""
+  },
+  {
+    imagem: "./assets/img/img-anime02.jpg",
+    titulo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eros tellus, malesuada et velit in, blandit molestie dolor.",
+    descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eros tellus, malesuada et velit in, blandit molestie dolor...",
+    data: "00/00/0000",
+    link: ""
+  },
+  {
+    imagem: "./assets/img/img-anime03.jpg",
+    titulo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eros tellus, malesuada et velit in, blandit molestie dolor.",
+    descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eros tellus, malesuada et velit in, blandit molestie dolor...",
+    data: "00/00/0000",
+    link: ""
+  },
+  {
+    imagem: "./assets/img/img-anime04.jpg",
+    titulo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eros tellus, malesuada et velit in, blandit molestie dolor.",
+    descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eros tellus, malesuada et velit in, blandit molestie dolor...",
+    data: "00/00/0000",
+    link: ""
+  },
+  {
+    imagem: "./assets/img/img-anime05.jpg",
+    titulo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eros tellus, malesuada et velit in, blandit molestie dolor.",
+    descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eros tellus, malesuada et velit in, blandit molestie dolor...",
+    data: "00/00/0000",
+    link: ""
+  },
+]
+
+listarScroll = () => {
+  let boxScroll = document.querySelector('.container-scroll')
+
+  scroll.map((valScroll) => {
+    boxScroll.innerHTML += `
+      <div class="single-scroll">
+        <figure class="img-scroll">
+          <div class="leg-scroll">
+            <span>Lorem</span>
+          </div>
+          <img src=`+ valScroll.imagem +` alt="Filmes">
+        </figure>
+        <div class="ds-scroll">
+          <h4>`+ valScroll.titulo +`</h4>
+          <p>`+ valScroll.descricao +`</p>
+          <span>`+ valScroll.data +`</span>
+          <a href=`+ valScroll.link +`>Ler notícia</a>
+        </div>
+      </div>
+    `;
+  })
+}
+
+listarScroll();
