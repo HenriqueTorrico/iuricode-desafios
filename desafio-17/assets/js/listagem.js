fa9ddde6429ab4b42fcea3db9686b17e78cb649e
HenriqueTorrico <henriquetorrico12@gmail.com>
2022-01-29 00:22:09 -0300

    desafios concluídos

---
+++ desafio-17/assets/js/listagem.js
@@ -0,0 +1,33 @@
+const usuarios = [
+  {
+    background: "tipo01",
+    nome: "Matheus Ramos",
+  },
+  {
+    background: "tipo02",
+    nome: "Iuri Silva",
+  },
+  {
+    background: "tipo03",
+    nome: "Vinicius Eiji",
+  },
+]
+
+listarUsuarios = () => {
+  let boxUsuarios = document.querySelector("#container-usuarios")
+
+  usuarios.map((valUsuarios) => {
+    boxUsuarios.innerHTML += `
+      <a href="./listagem.html">
+        <div class="single-usuarios">
+          <div class="img-usuarios `+ valUsuarios.background +`"></div>
+          <div class="ds-usuarios">
+            <h3>`+ valUsuarios.nome +`</h3>
+          </div>
+        </div>
+      </a>
+    `;
+  })
+}
+
+listarUsuarios();
