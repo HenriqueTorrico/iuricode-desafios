62796edf9208160243d8b85e82467161351d5f96
HenriqueTorrico <henriquetorrico12@gmail.com>
2022-02-19 09:50:54 -0300

    desafios concluídos

---
+++ desafio-21/README.md
@@ -0,0 +1,36 @@
+# Desafio 21 - IuriCode (Xbox) 💻
+
+<br>
+
+## 📌 Sobre
+
+### 🥇 Vigésimo primeiro projeto dos desafios propostos por  <a href="https://www.linkedin.com/in/iuricode/" target="_blank">IuriCode</a> no qual ele fornece o layout projetado no Figma.
+
+<br>
+
+## 🎯 Projeto
+
+<br>
+
+<img src="./assets/design/xbox.jpg">
+
+<br>
+
+## ✨ Links úteis
+
+### ⚫ Design do projeto: <a href="https://www.figma.com/file/Yb9IBH56g7T1hdIyZ3BMNO/Desafios---Codelândia?node-id=64381%3A758" target="_blank">Clique aqui</a>
+### ⚫ Criador do desafio: <a href="https://www.linkedin.com/in/iuricode/" target="_blank">Clique aqui</a>
+  
+<br>
+
+## 💻 Tecnologias utilizadas
+
+### ✅ HTML
+### ✅ CSS
+### ✅ JavaScript
+
+<br>
+
+## 🎉 Resultado
+
+### 🧐 Confira o resultado clicando <a href="https://xbox-torrico.netlify.app" target="_blank">aqui</a>
--- desafio-21/assets/js/main.js
+++ desafio-21/assets/js/main.js
@@ -1,3 +1,38 @@
+const perguntas = [
+  {
+    pergunta: "Meus jogos anteriores do Xbox vão funcionar no Xbox Series X?",
+    resposta: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    pergunta: "O que está incluído no Xbox Series X?",
+    resposta: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    pergunta: "Como sei se minha TV é compatível com 4K?",
+    resposta: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  }
+]
+
+listarPerguntas = () => {
+  let boxPergunta = document.querySelector(".container-perguntas")
+
+  perguntas.map((valPegunta) => {
+    boxPergunta.innerHTML += `
+      <div class="single-perguntas">
+        <div class="pergunta">
+          <h3>`+ valPegunta.pergunta +`</h3>
+          <img src="./assets/img/arrow.svg" alt="Ícone arrow">
+        </div>
+        <div class="resposta">
+          <p>`+ valPegunta.resposta +`</p>
+        </div>
+      </div>
+    `;
+  })
+}
+
+listarPerguntas()
+
 const single_perguntas = document.querySelectorAll(".single-perguntas")
 
 single_perguntas.forEach((pergunta) => {
